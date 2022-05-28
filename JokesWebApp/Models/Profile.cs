444b2d556316ed65bfc0d51336ee894b7607260c
h1to <murzaliev.com@gmail.com>
2022-05-28 13:13:32 +0600

    Add project files.

---
+++ JokesWebApp/Models/Profile.cs
@@ -0,0 +1,14 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Profile
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PasswordX2 { get; set; }
+        public Profile()
+        {
+
+        }
+    }
+}
